fb5fce2d743c3c3054141481c46046c091dbbf1f
olivia <olivia.brantley9@gmail.com>
2023-03-03 14:11:20 -0800

    added components folder and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+
+import Form from './components/Form'
+import Resume from './components/Resume'
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Form />
+      <Resume />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Form(){
+    return(
+        <div>This is the form component</div>
+    )
+}
+
+export default Form
