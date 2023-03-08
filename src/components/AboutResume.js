ed0ce3a824594b986e7c13e33c33c2f072eee06c
olive <olivia.brantley9@gmail.com>
2023-03-08 14:17:42 -0800

    added bullet points

--- src/components/AboutResume.js
+++ src/components/AboutResume.js
@@ -5,14 +5,14 @@ function AboutResume({aboutInfo}){
     return(
         <div>
             <div className="full-name">
-                <div>{aboutInfo.firstName}</div>
-                <div>{aboutInfo.lastName}</div>
+                <div> {aboutInfo.firstName}</div>
+                <div> {aboutInfo.lastName}</div>
             </div>
-
+            <div className="bar-deco"></div>
             <div className="contact-info">
-                <div>{aboutInfo.email}</div>
-                <div>{aboutInfo.phoneNumber}</div>
-                <div>{aboutInfo.linkedIn}</div>
+                <div>&#x2022; {aboutInfo.email}</div>
+                <div>&#x2022; {aboutInfo.phoneNumber}</div>
+                <div>&#x2022; {aboutInfo.linkedIn}</div>
             </div>
         </div>
     )
