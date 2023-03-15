16b5fdb4b4e78385940f5ac75e68b8f12cd83e01
olivia <olivia.brantley9@gmail.com>
2023-03-15 11:00:39 -0700

    changed render to map

--- src/components/ExpResume.js
+++ src/components/ExpResume.js
@@ -4,22 +4,28 @@ import './Resume.css';
 function ExpResume({exps}){
     return(
         <div>
-
             <div className="exp-resume">
                 <div className="exp-resume-title">EXPERIENCE</div>
                 <div className="bar-deco"></div>
             </div>
 
-            <div className="exp-div">
-            <div className="exp-top-line">
-                    <div className="exp-company">Company</div>
-                    <div>Location</div>
-            </div>
-            <div className="exp-middle">
-                    <div className="exp-job-title">{exps.jobTitle}</div>
-                    <div>Start Date - End Date</div>
-            </div>
-            <div className="exp-text">Text Box area</div>
+            <div>
+                {exps.map(exp =>(
+                    <div key={exp.id}>
+                        <div className="exp-div">
+                        <div className="exp-top-line">
+                                <div className="exp-company">Company</div>
+                                <div>Location</div>
+                        </div>
+                        <div className="exp-middle">
+                                <div className="exp-job-title">{exp.jobTitle}</div>
+                                <div>Start Date - End Date</div>
+                        </div>
+                        <div className="exp-text">Text Box area</div>
+                        </div>
+                    </div>
+                ))}
+              
             </div>
         </div>
     )
