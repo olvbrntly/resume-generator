0ec37e821699c3a5a1aa07c059d7abf64a4b6fe7
olivia <olivia.brantley9@gmail.com>
2023-03-14 17:03:45 -0700

    basic Exp resume div

---
+++ src/components/ExpResume.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Resume.css';
+
+function ExpResume(){
+    return(
+        <div>
+           <div>
+                <div>Company</div>
+                <div>Location</div>
+           </div>
+           <div>
+                <div>Job Title</div>
+                <div>Start Date - End Date</div>
+           </div>
+           <div>Text Box area</div>
+        </div>
+    )
+}
+
+export default ExpResume
